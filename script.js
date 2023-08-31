1b9bb045da262ec3ab69c4e49ce701ece205621b
Prasad Jadhav <86917788+JadhavPrasad21@users.noreply.github.com>
2023-08-31 20:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Your code here.
+const items = document.querySelector('.items');
+let isDragging = false;
+let startPosition = 0;
+let scrollLeft = 0;
+
+items.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startPosition = e.clientX;
+  scrollLeft = items.scrollLeft;
+
+  items.classList.add('active');
+});
+
+items.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+
+  const distance = e.clientX - startPosition;
+  items.scrollLeft = scrollLeft - distance;
+});
+
+items.addEventListener('mouseup', () => {
+  isDragging = false;
+  items.classList.remove('active');
+});
+
+items.addEventListener('mouseleave', () => {
+  isDragging = false;
+  items.classList.remove('active');
+});
